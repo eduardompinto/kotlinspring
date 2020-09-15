a2c226fe5d3feee38f3cc4a6f2dde55f46d33dcc
Eduardo Martins Pinto <eduardo.90.martins@gmail.com>
2020-09-15 13:06:58 +0200

    Initial

---
+++ src/main/kotlin/com/eduardompinto/kotlinspring/KotlinspringApplication.kt
@@ -0,0 +1,11 @@
+package com.eduardompinto.kotlinspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinspringApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinspringApplication>(*args)
+}
