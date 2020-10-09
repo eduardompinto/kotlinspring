3273d42f4976aa77db1c3ad88a0756499eab6997
Eduardo Martins Pinto <eduardo.90.martins@gmail.com>
2020-10-09 21:30:40 +0200

    Non web project using kotlin serialization

--- src/main/kotlin/com/eduardompinto/kotlinspring/usecase/githubstarred/GithubStarredFetcherSequential.kt
+++ src/main/kotlin/com/eduardompinto/kotlinspring/usecase/githubstarred/GithubStarredFetcherSequential.kt
@@ -1,23 +1,34 @@
 package com.eduardompinto.kotlinspring.usecase.githubstarred
 
+
+import com.eduardompinto.kotlinspring.get
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 @Service
 class GithubStarredFetcherSequential(
-    private val restTemplate: RestTemplate
+    private val httpClient: HttpClient,
+    private val httpRequestBuilder: HttpRequest.Builder
 ) : GithubStarredFetcher {
 
     override fun fetchStars(user: String): List<StarredResponse> {
         var url: String? = STARRED_URLS.format(user, 1)
         val starredRepos = mutableListOf<StarredResponse>()
         while (url != null) {
-            val resp = restTemplate.getForEntity(
-                url,
-                Array<StarredResponse>::class.java
-            )
-            url = Link.getNextLinkUrl(resp.headers["link"])
-            starredRepos.addAll(resp.body?.toList() ?: emptyList())
+            val request = httpRequestBuilder
+                .uri(URI(url))
+                .GET()
+                .build()
+            val resp = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
+            url = Link.getNextLinkUrl(resp.headers()["link"])
+            if (resp.body() != null) {
+                starredRepos.addAll(Json.decodeFromString(resp.body()))
+            }
         }
         return starredRepos
     }
